ee86fcc286e6274405a464dc64ca8490c183311b
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-25 10:53:01 +0545

    practice qn completed!

--- PRACTICE QNS/Practice/src/array/PracticeQn.java
+++ PRACTICE QNS/Practice/src/array/PracticeQn.java
@@ -2,7 +2,7 @@ package array;
 
 import java.util.Arrays;
 import java.util.Scanner;
-// solution optimized
+
 public class PracticeQn {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
