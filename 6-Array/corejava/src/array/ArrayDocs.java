72af6ba77a552a2970d01c17cb393a4368012f9a
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-28 18:01:23 +0545

    Array theory Docs created!

---
+++ 6-Array/corejava/src/array/ArrayDocs.java
@@ -0,0 +1,46 @@
+package array;
+
+public class ArrayDocs {
+	/*
+	 *  ------- Array --------
+	 *  
+	 *  	int a = 400;
+	 *  		a = 200;
+	 *  
+	 *  		sysout(a); //200
+	 *  
+	 *  	x = 555, y = 666, z = 874 
+	 *  	what if we were to store 500 values? use array!
+	 *  
+	 *  	REM!!!!
+	 *  	array variable = static collection 
+	 *  
+	 *  	multiple values, single data type and finite collection
+	 *  
+	 *  	ARRAY: Collection of homogeneous data
+	 *  		   Collection/group of multiple values of same data type with fixed size
+	 *  		   Contains duplicate data
+	 *  		   Data stored on the basis of indices(index starts from 0)
+	 *  		   Random-access is possible
+	 *  		   Contiguous memory allocation
+	 *  
+	 *  		   REM!!! Array is an object in Java!
+	 *  
+	 *  # SYNTAX:
+	 *  
+	 *  		data_type arr_name[] = new data_type[size];
+	 *  
+	 *  		e.g.
+	 *  			float height[] = new float[200];
+	 *  
+	 *  			String[] cname = new String[50];
+	 *  
+	 *  			char []alphas = new char[26];
+	 *  
+	 *  			Employee emps[] = new Employee[500]; // class as user-defined type
+	 *  
+ 	 */
+	public static void main(String[] args) {
+		
+	}
+}
