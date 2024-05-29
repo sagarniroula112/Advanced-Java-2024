4566697cb9231c5cc03e5c24ae0a501c1e737310
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-29 16:30:05 +0545

    changes to practice class arraywithmethods!

--- PRACTICE QNS/Practice/src/array/ArrayWithMethods.java
+++ PRACTICE QNS/Practice/src/array/ArrayWithMethods.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class ArrayWithMethods {
 //	29 May 2024
 	/*
@@ -24,6 +26,12 @@ public class ArrayWithMethods {
 		sum(num);
 		String[] cities = {"Kathmandu", "Delhi", "Moscow", "Washington", "Dhaka"};
 		printCities(cities);
+		int[] arr = fetchOddNumbersFrom1To100();
+//		for(int i=0;i<50;i++) {
+//			System.out.println(arr[i]+ " ");
+//		}
+		// Instead to print only, do this!!!!!
+		System.out.println(Arrays.toString(arr));
 	}
 	
 	// -- Array as the argument --
@@ -40,5 +48,20 @@ public class ArrayWithMethods {
 		for(String city: cities) {
 			System.out.print(city+" ");
 		}
+		System.out.println(" ");
+	}
+	
+	// -- Array as a return type --
+	static int[] fetchOddNumbersFrom1To100() {
+		int oddNums[] = new int[50];
+		int j = 0;
+		
+		for(int i=1;i<=100;i++) {
+			if(i%2!=0) {
+				oddNums[j] = i;
+				j++;
+			}
+		}
+		return oddNums;
 	}
 }
