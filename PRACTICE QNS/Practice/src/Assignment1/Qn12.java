f5102fee2eba0f6c31e199e7e6ad3dee3260d454
Sagar Niroula <sagarniroula112@gmail.com>
2024-05-31 11:17:40 +0545

    practice qn 12 done!

---
+++ PRACTICE QNS/Practice/src/Assignment1/Qn12.java
@@ -0,0 +1,8 @@
+package Assignment1;
+
+public class Qn12 {
+	/*
+	 * Write a program to accept the roll, name, and nationality of the person 
+	 * and display those values in good format way.
+	 */
+}
