c48fd24d0f7f943d20ac26c25e70af94c44ab608
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-03 17:26:02 +0545

    more assignments questions done!

--- PRACTICE QNS/Practice/src/Assignment1/Qn13.java
+++ PRACTICE QNS/Practice/src/Assignment1/Qn13.java
@@ -1,5 +1,22 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class Qn13 {
-    // Commit for June 1
+	/*
+	 * Write a program to print the number entered by user only if the number entered is 
+		negative.
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number:: ");
+		int num = sc.nextInt();
+		
+		if(num<0) {
+			System.out.println("Number entered was: " + num);
+		}else {
+			System.out.println("It was a positive number.");
+		}
+	}
 }
