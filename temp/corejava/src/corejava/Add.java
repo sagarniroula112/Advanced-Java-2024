549b3829ba4919ec09e84b06d6be4f3ce6b0ae05
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-04 19:34:23 +0545

    cleaning!

--- temp/corejava/src/corejava/Add.java
+++
@@ -1,9 +0,0 @@
-package corejava;
-
-public class Add {
-	public static void main(String[] args) {
-		int a = 45, b = 45;
-		int s = a + b;
-		System.out.println("Addition of a and b: "+s);
-	}
-}
--- temp/corejava/src/corejava/MyInfo.java
+++
@@ -1,9 +0,0 @@
-package corejava;
-
-public class MyInfo {
-	public static void main(String[] args) {
-		System.out.println("Hello from Broadway!");
-		System.out.println("I am learning Java Backend!!");
-		System.out.println("We are strong together!!!!!");
-	}
-}
