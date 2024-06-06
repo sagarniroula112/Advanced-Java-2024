cdb9c3da968c5b78a308adabc94f8e2985928c2e
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-06 21:11:26 +0545

    Inheritance tested!

---
+++ 10-Encapsulation&Inheritance/corejava/src/Inheritance/Test.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class Test {
+	public static void main(String[] args) {
+		Programmer p = new Programmer();
+		
+		p.id = 669;
+		p.name = "Pranav";
+		p.company = "Google";
+		p.project = "G-earth";
+		p.salary = 800000;
+		p.bonus = 50000;
+		p.progLang = "Java";
+		p.print();
+	}
+}
+
+// creating objects in array using loop....
