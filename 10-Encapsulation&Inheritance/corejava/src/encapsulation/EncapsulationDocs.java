f29f6e5a9a3b4f011aeba52e80c95123dab0a8e2
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-06 21:02:27 +0545

    Encapsulation docs added!

---
+++ 10-Encapsulation&Inheritance/corejava/src/encapsulation/EncapsulationDocs.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class EncapsulationDocs {
+	/*
+	 * ----------- Encapsulation ------------
+	 * 
+	 * # process of data hiding(class ko property lai bahira nadekhau) and data binding
+	 * # to achieve encapsulation:
+	 * 		a> Declare properties of class as private(i.e. data hiding)
+	 * 		b> Create public getters and setters methods to get and set values to private properties
+	 * 			(data binding)
+	 * 
+	 * # Syntax:
+	 * 	
+	 * 		// POJO or Model or Entity class(Business Logic navako, plain data matrai vako)
+	 * 
+	 * 		class class_name{
+	 * 			// private properties
+	 * 
+	 * 			// public getters and setters methods
+	 * 		}
+	 * 
+	 */
+}
