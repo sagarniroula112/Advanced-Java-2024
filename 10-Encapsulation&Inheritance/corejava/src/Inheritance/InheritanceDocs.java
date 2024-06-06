b9a87cfa85e82c4733d5f4875608378300a5e1d8
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-06 21:10:42 +0545

    Inheritance docs added!

---
+++ 10-Encapsulation&Inheritance/corejava/src/Inheritance/InheritanceDocs.java
@@ -0,0 +1,38 @@
+package Inheritance;
+
+public class InheritanceDocs {
+	
+	/*
+	 * ---------- Inheritance -----------
+	 * 
+	 * 	# code re-usability
+	 * 	# process of accessing properties and methods of parent/super/base class to child/sub/derived
+	 * 		class is known as inheritance
+	 * 
+	 * 	# Also known as IS-A relationship.
+	 * 	# It's a generalization to specialization process. (parent to child)
+	 * 	# We can inherit only one class at a time. (REMEMBER!!!)
+	 * 	# We can't inherit private properties, methods and constructors. (REMEMBER!!!)
+	 * 
+	 * 	# Syntax:
+	 * 	
+	 * 			child_class extends parent_class{
+	 * 				// properties: child + parent
+	 * 
+	 * 				// methods: child + parent
+	 * 			}
+	 * 		
+	 * 		e.g. 
+	 * 			Class A{
+	 * 			
+	 * 			}
+	 * 
+	 * 			Class B extends A{
+	 * 			
+	 * 			}
+	 * 
+	 * 			Class C extends B{
+	 * 			
+	 * 			}
+	 */
+}
