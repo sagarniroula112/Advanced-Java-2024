439e80b58bbb2e04b9818cedfc058a6a3e1c5698
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-08 19:36:37 +0545

    Creation of central bank as parent class!

---
+++ 12-MethodOverloading&Aggregation/corejava/src/methodoverriding/CentralBank.java
@@ -0,0 +1,17 @@
+package methodoverriding;
+
+public class CentralBank {
+	void getBankName() {
+		System.out.println("Central Bank");
+	}
+	
+	void getInterestRate() {
+		System.out.println("0");
+	}
+	
+	void moneyExRate() {
+		System.out.println("$1 = 132");
+		System.out.println("Yen 10 = 10");
+		System.out.println("KDD 1 = 16");
+	}
+}
