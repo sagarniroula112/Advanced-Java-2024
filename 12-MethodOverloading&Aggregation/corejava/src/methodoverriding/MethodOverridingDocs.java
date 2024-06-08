7bc16720978629f5ddcf923fb5bcc2fc2769f3ff
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-08 19:36:07 +0545

    Method Overriding notes updated!

---
+++ 12-MethodOverloading&Aggregation/corejava/src/methodoverriding/MethodOverridingDocs.java
@@ -0,0 +1,18 @@
+package methodoverriding;
+
+public class MethodOverridingDocs {
+	/*
+	 * # Overloading and overriding are for run-time polymorphism.
+	 * # There must be inheritance between two classes.
+	 * 
+	 * # Method Overloading -> Eutai kaam dherai tarika bata garne ( is-a relation, parent class ko method chai child ma modify garne )
+	 * # But inherit chai as it is use garne
+	 * 
+	 * # method name, return type and list of arguments same as parent class but implementation is different in child class, is known
+	 *   as method overriding.
+	 *   ----- Can you change value of the methods? --- Ans: Method overriding garne (yasma pani final chalxa)
+	 *   ----- Can you change value of the class? --- Ans: Inheritance (change garna nadina "final" bananune(inherit garna bata rokne)
+	 */
+	
+	
+}
