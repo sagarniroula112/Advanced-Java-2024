17d96bb0f0dbc44de7f874eabf5f98b4af13b6d1
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-11 19:35:41 +0545

    JavaCollection Docs created!

---
+++ 14-JavaCollection/corejava/src/javacollection/JavaCollectionDocs.java
@@ -0,0 +1,24 @@
+package javacollection;
+
+public class JavaCollectionDocs {
+	
+	/*
+	 * -------------- Collection Framework/Java Collection ------------ (collection sanga kaam garne api haru)
+	 * 
+	 * used to store and process dynamic multiple data/objects
+	 * 
+	 * 1> List : dynamic array (runtime size set)
+	 * 		a> ArrayList : slow processing, stack ma store garxa, better to use for data storage
+	 * 		b> LinkedList : fast processing, better to use for data processing
+	 * 
+	 * 2> Set : collection of unique data
+	 * 		a> HashSet : unique data only (no order)
+	 * 		b> TreeSet : unique + sorted data
+	 * 		c> LinkedHashSet : unique + insertion in order
+	 * 
+	 * 3> Map : data manage in key-value pairs
+	 * 		a> HashMap : <k, v> pair
+	 * 		b> TreeMap : <k, v> + sorted
+	 * 		c> LinkedHashMap : <k, v> + insertion in order
+	 */
+}
