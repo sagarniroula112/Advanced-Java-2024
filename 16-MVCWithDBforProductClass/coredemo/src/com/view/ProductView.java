f3930c1aca9538d16d3ce3eb7ff276b13a171ddf
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-16 20:50:15 +0545

    Necessary comments added!

--- 16-MVCWithDBforProductClass/coredemo/src/com/view/ProductView.java
+++ 16-MVCWithDBforProductClass/coredemo/src/com/view/ProductView.java
@@ -12,22 +12,22 @@ public class ProductView {
 		addBtn();
 		getAll();
 		delete();
-		
+
 		// show products AFTER DELETED
-		
-//		getAll();
+
+		// getAll();
 	}
-	
+
 	// add product
 	static void addBtn() {
 		ProductService pService = new ProductServiceImpl();
-		
+
 		char flag = 'y';
 		Scanner sc = new Scanner(System.in);
-		
+
 		do {
 			Product p = new Product();
-		
+
 			System.out.println("Enter Product ID:: ");
 			p.setId(sc.nextInt());
 			System.out.println("Enter Product Name:: ");
@@ -36,27 +36,28 @@ public class ProductView {
 			p.setCompany(sc.next());
 			System.out.println("Enter Product Price:: ");
 			p.setPrice(sc.nextInt());
-		
+
 			pService.addProduct(p);
-			
+
 			System.out.println("Do you want to add more [y/n]? ");
 			flag = sc.next().charAt(0);
-		} while(flag == 'y');
+		} while (flag == 'y');
 	}
-	
+
 	// get all products
 	static void getAll() {
 		ProductService pService = new ProductServiceImpl();
-		
+
 		List<Product> pList = pService.getAllProducts();
-		
+
 		System.out.println(pList);
 	}
-	
+
+	// add new prompt to user for id to delete
 	static void delete() {
 		Scanner sc = new Scanner(System.in);
 		ProductService pService = new ProductServiceImpl();
-		
+
 		System.out.println("Enter ID of product you want to delete:: ");
 		int id = sc.nextInt();
 		pService.deleteProduct(id);
