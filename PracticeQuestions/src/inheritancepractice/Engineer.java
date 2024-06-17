e08ae4187c0de392247873adce91ae0e287a8d11
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-17 14:13:40 +0545

    Fully implemented Engineer class!

--- PracticeQuestions/src/inheritancepractice/Engineer.java
+++ PracticeQuestions/src/inheritancepractice/Engineer.java
@@ -1,4 +1,13 @@
 package inheritancepractice;
 
 public class Engineer extends Employee{
+
+    public Engineer(int empId, String name, String ssn, double salary) {
+        super(empId, name, ssn, salary);
+    }
+
+    public void printEmployee(){
+        super.printEmployee();
+        System.out.println("Additional Employee Details HERE!!");
+    }
 }
