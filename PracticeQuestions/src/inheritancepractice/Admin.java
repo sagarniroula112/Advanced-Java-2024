a6c659a0e2ee6d91c76c2c32e781c923ca864238
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-17 14:12:09 +0545

    Fully implemented Admin and Director Class!

--- PracticeQuestions/src/inheritancepractice/Admin.java
+++ PracticeQuestions/src/inheritancepractice/Admin.java
@@ -1,4 +1,12 @@
 package inheritancepractice;
 
 public class Admin extends Employee {
+
+    public Admin(int empId, String name, String ssn, double salary) {
+        super(empId, name, ssn, salary);
+    }
+
+//    public void printEmployee(){
+//        super.printEmployee();
+//    }
 }
--- PracticeQuestions/src/inheritancepractice/Director.java
+++ PracticeQuestions/src/inheritancepractice/Director.java
@@ -1,9 +1,19 @@
 package inheritancepractice;
 
-public class Director extends Manager{
+public class Director extends Manager {
     private double budget;
+
+    public Director(int empId, String name, String ssn, double salary, String deptName, double budget) {
+        super(empId, name, ssn, salary, deptName);
+        this.budget = budget;
+    }
 
     public double getBudget(){
         return this.budget;
+    }
+
+    public void printEmployee(){
+        super.printEmployee();
+        System.out.println("Budget: " + this.budget);
     }
 }
