73f807aa2402c9ab474fda6b6dd8e97a5f39d7c8
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-17 14:13:25 +0545

    Fully implemented Employee class!

--- PracticeQuestions/src/inheritancepractice/Employee.java
+++ PracticeQuestions/src/inheritancepractice/Employee.java
@@ -1,44 +1,57 @@
 package inheritancepractice;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Employee {
     private int empId;
     private String name;
     private String ssn;
     private double salary;
 
+    public Employee(int empId, String name, String ssn, double salary){
+        this.empId = empId;
+        this.name= name;
+        this.ssn=ssn;
+        this.salary = salary;
+    }
+
     public void setName(String name){
+        if(name==null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty!");
+        }
         this.name = name;
     }
 
     public void raiseSalary(double increase){
-        this.salary = (1+(increase/100) * this.salary);
+        if(increase<0){
+            throw new IllegalArgumentException("Increment can't be negative!");
+        }
+        this.salary = (increase * this.salary);
+//        System.out.println("Increased Salary: " + this.salary);
     }
 
     public int getEmpId() {
         return empId;
     }
 
-    public void setEmpId(int empId) {
-        this.empId = empId;
-    }
-
     public String getSsn() {
         return ssn;
-    }
-
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
     }
 
     public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void printEmployee(){
+        String formattedSalary = NumberFormat.getCurrencyInstance(Locale.US).format(this.salary);
+        System.out.println("ID: " + this.getEmpId());
+        System.out.println("Name: " + this.getName());
+        System.out.println("SSN: " + this.getSsn());
+        System.out.println("Salary: " + formattedSalary);
     }
 }
