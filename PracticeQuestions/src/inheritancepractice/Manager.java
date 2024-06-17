1304533f376c0d20b41fa574c36239bb4f340093
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-17 14:13:52 +0545

    Fully implemented Manager class!

--- PracticeQuestions/src/inheritancepractice/Manager.java
+++ PracticeQuestions/src/inheritancepractice/Manager.java
@@ -3,7 +3,17 @@ package inheritancepractice;
 public class Manager extends Employee{
     private String deptName;
 
+    public Manager(int empId, String name, String ssn, double salary, String deptName) {
+        super(empId, name, ssn, salary);
+        this.deptName = deptName;
+    }
+
     public String getDeptName(){
         return this.deptName;
+    }
+
+    public void printEmployee(){
+        super.printEmployee();
+        System.out.println("DepartmentName: " + this.deptName);
     }
 }
