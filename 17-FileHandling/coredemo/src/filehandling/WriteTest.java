75b606cb0a078566754cd10576162d502201aa5c
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-23 18:23:50 +0545

    file write done!

---
+++ 17-FileHandling/coredemo/src/filehandling/WriteTest.java
@@ -0,0 +1,24 @@
+package filehandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteTest {
+	/*
+	 * a> FileWriter : to write file
+	 * b> FileReader : to read file
+	 */
+	
+	public static void main(String[] args) throws IOException {
+		FileWriter fw = new FileWriter("d://employee.txt", true);
+		
+		fw.write("ID : 999\n");
+		fw.write("Name : Ram KC \n");
+		fw.write("Age : 19 \n");
+		fw.write("City : Jhapa \n");
+		fw.write("Phone : 558844552\n");
+		
+		fw.close();
+		System.out.println("Success");
+	}
+}
