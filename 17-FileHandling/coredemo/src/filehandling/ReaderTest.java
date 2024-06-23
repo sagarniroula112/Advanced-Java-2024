48470d234559ba912dc3472b99e62338a27ecc05
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-23 18:24:06 +0545

    file reader class docs!

---
+++ 17-FileHandling/coredemo/src/filehandling/ReaderTest.java
@@ -0,0 +1,24 @@
+package filehandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderTest {
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("d://employee.txt"); 
+		BufferedReader br = new BufferedReader(fr);  // loading to cache memory
+		
+		String line;
+		while((line = br.readLine()) != null) {
+			System.out.println(line);
+			
+			String[] data = line.split(",");
+			
+			System.out.println(data[1]);
+			
+			// try
+			// 1. db connection, 2. write sql (insert), 3. set values to sql, 4. execute sql
+		}
+	}
+}
