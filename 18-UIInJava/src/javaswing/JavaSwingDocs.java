2e0fbe8ef645bed8a15f03bc7a9c311a87dcc2c3
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-24 18:42:28 +0545

    Java Swing Docs!

---
+++ 18-UIInJava/src/javaswing/JavaSwingDocs.java
@@ -0,0 +1,11 @@
+package javaswing;
+
+public class JavaSwingDocs {
+	/*
+	 * ----------- Swing Framework ------------
+	 * # used to create UI/GUI for desktop application in Java.
+	 * 
+	 * # UI components:
+	 *   -> JFrame
+	 */
+}
