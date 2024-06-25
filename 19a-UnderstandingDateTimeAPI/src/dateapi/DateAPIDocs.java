b395263a43f551bde96839c55da4c175f26a928e
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-25 13:09:51 +0545

    TimeAPI docs added!

---
+++ 19a-UnderstandingDateTimeAPI/src/dateapi/DateAPIDocs.java
@@ -0,0 +1,21 @@
+package dateapi;
+
+public class DateAPIDocs {
+    /*
+    Java Time API, is a comprehensive set of classes for handling dates, times, and durations.
+
+    Introduced in Java 8, it provides a more powerful and flexible way to work with date and
+    time compared to the old java.util.Date and java.util.Calendar classes.
+
+    Key Classes in Java Time API:
+
+    LocalDate: Represents a date (year, month, day) without time.
+    LocalTime: Represents a time (hour, minute, second, nanosecond) without a date.
+    LocalDateTime: Combines date and time without time zone information.
+    ZonedDateTime: Combines date and time with time zone information.
+    Period: Represents a period of time in terms of years, months, and days.
+    Duration: Represents a duration of time in terms of seconds and nanoseconds.
+    Instant: Represents a point in time (timestamp) from the epoch (1970-01-01T00:00:00Z).
+
+     */
+}
