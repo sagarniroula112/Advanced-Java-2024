51bbd7ee67d7374ea572c6645e6606d81fef6f51
Sagar Niroula <sagarniroula112@gmail.com>
2024-06-26 15:25:58 +0545

    Exception handling docs added!

---
+++ 20a-ExceptionHandlingInJava/src/ExceptionHandlingDocs.java
@@ -0,0 +1,18 @@
+public class ExceptionHandlingDocs {
+    /*
+    -> An exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions.
+    -> When an exception occurs, Java creates an exception object that contains information about the error.
+
+    -> Types of Exceptions:
+        a. Checked Exceptions: Checked at compile-time. Eg. IOException, SQLException
+        b. Unchecked Exceptions: Not checked at compile-tile but occur during runtime. Eg. ArithmeticException, NullPointerEx.
+        c. Errors: Problems beyond control of the user or the programmer. Eg. OutOfMemoryError, StackOverflowError
+
+    -> Exception Handling Keywords:
+        a. try - code that might throw an exception
+        b. catch - catches and handles the exception
+        c. finally - code that will run regardless of whether an exception is thrown or not
+        d. throw - used to throw an exception explicitly
+        e. throws - declares the exceptions that a method might throw
+     */
+}
