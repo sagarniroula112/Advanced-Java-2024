31fea0ce8f35426ef2e55fab26bd6ed25472ea2f
Sagar Niroula <sagarniroula112@gmail.com>
2024-07-06 10:24:26 +0545

    commit!

--- 17-FileHandling/coredemo/src/filehandling/WriteTest.java
+++ 17-FileHandling/coredemo/src/filehandling/WriteTest.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteTest {
-	/*
+	/* WriteTest methods
 	 * a> FileWriter : to write file
 	 * b> FileReader : to read file
 	 */
